d71c421b86a81d6d89e3e4a4f0c96be19ff2d3eb
f3ve <free.eve@pm.me>
2020-11-15 04:15:22 -0800

    almost finished styling topbar

--- components/global/RouterLinks.js
+++ components/global/RouterLinks.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { Link } from '@material-ui/core';
 
-const RouterLink = ({ href, color, text }, props) => {
+const RouterLink = ({ href, color, text, className }, props) => {
   const router = useRouter();
 
   const handleLinkClick = (e) => {
@@ -9,7 +9,13 @@ const RouterLink = ({ href, color, text }, props) => {
     router.push(href);
   };
   return (
-    <Link href={href} color={color} {...props} onClick={handleLinkClick}>
+    <Link
+      href={href}
+      color={color}
+      {...props}
+      onClick={handleLinkClick}
+      className={className}
+    >
       {text}
     </Link>
   );
--- components/global/TopBar.js
+++ components/global/TopBar.js
@@ -1,18 +1,56 @@
 import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 import RouterLink from './RouterLinks';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(2),
+  },
+}));
+
 const TopBar = () => {
+  const styles = useStyles();
+
   return (
-    <AppBar style={{ backgroundColor: '#000000' }}>
-      <Toolbar>
-        <Typography variant='h3'>Freedom Evenden</Typography>
-        <IconButton color='secondary'>
-          <MenuIcon color='inherit' />
-        </IconButton>
-        <RouterLink color='secondary' href='/' text='yeet' />
-      </Toolbar>
-    </AppBar>
+    <div className={styles.root}>
+      <AppBar style={{ backgroundColor: '#000000' }}>
+        <Toolbar>
+          <IconButton
+            color='secondary'
+            edge='start'
+            className={styles.menuButton}
+          >
+            <MenuIcon color='inherit' />
+          </IconButton>
+          <Typography variant='h3' className={styles.title}>
+            Freedom Evenden
+          </Typography>
+          <RouterLink
+            color='secondary'
+            href='/'
+            text='yeet'
+            className={styles.link}
+          />
+          <RouterLink
+            color='secondary'
+            href='/blog'
+            text='blog'
+            className={styles.link}
+          />
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
