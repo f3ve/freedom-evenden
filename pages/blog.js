57812e0052c8b723c5602a929a2bbc58a78617b5
f3ve <free.eve@pm.me>
2020-12-13 12:01:05 -0800

    added some error handling to blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,41 +1,48 @@
-import { Container, Fade, Grid, GridList } from '@material-ui/core';
+import { Box, Collapse, Container, Fade, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { useEffect, useState } from 'react';
 import ArticleCard from '../components/home/ArticleCard';
 import { apiGet } from '../services/ArticleApiService';
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles((theme) => ({
+  box: {
+    margin: theme.spacing(2),
+  },
+}));
 
 const blog = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
   const styles = useStyles();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     apiGet('articles/?page_size=20')
       .then((res) => setData(res))
-      .catch((err) => setError(err));
+      .catch((err) => {
+        console.log('An error occured:', err);
+        setError(true);
+      });
   }, []);
 
   return (
-    <Fade in={!!data}>
+    <Fade in>
       <Container maxWidth='md'>
-        <Grid
-          container
-          spacing={3}
-          alignContent='space-around'
-          className={styles.gridContainer}
-          justify='space-between'
-        >
-          {data &&
-            data.results.map((article) => {
-              return (
-                <Grid item key={article.id} xs={4}>
+        <Collapse in={error}>
+          <Typography color='error'>
+            Uh oh! Looks like something went wrong. Refresh the page or try
+            again later.
+          </Typography>
+        </Collapse>
+        {data &&
+          data.results.map((article) => {
+            return (
+              <>
+                <Box className={styles.box}>
                   <ArticleCard article={article} />
-                </Grid>
-              );
-            })}
-        </Grid>
+                </Box>
+              </>
+            );
+          })}
       </Container>
     </Fade>
   );
