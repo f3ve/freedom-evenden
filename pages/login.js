7c998b464be76e2d7eee1e6a39de7c5ffef0060d
f3ve <free.eve@pm.me>
2020-12-17 16:45:19 -0800

    set up login page

---
+++ pages/login.js
@@ -0,0 +1,54 @@
+import {
+  Box,
+  Button,
+  Container,
+  makeStyles,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { useState } from 'react';
+
+const useStyles = makeStyles(() => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
+
+const login = () => {
+  const styles = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Container maxWidth='md'>
+      <Typography variant='h2' color='secondary' align='center'>
+        Login
+      </Typography>
+      <Box component='form' className={styles.form}>
+        <TextField
+          label='Email'
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label='password'
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          color='primary'
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(email, password);
+          }}
+        >
+          Log in
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default login;
