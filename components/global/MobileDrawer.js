bc34d53a552e29d16fa18a6766c5fdaaf1516495
f3ve <free.eve@pm.me>
2021-01-06 18:12:44 -0800

    refined the mobile drawer component;

--- components/global/MobileDrawer.js
+++ components/global/MobileDrawer.js
@@ -1,5 +1,17 @@
-import { Divider, Drawer } from '@material-ui/core';
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+} from '@material-ui/core';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import DescriptionIcon from '@material-ui/icons/Description';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import { makeStyles } from '@material-ui/core/styles';
+import { useRouter } from 'next/router';
 import { colors } from '../../Theme';
 import RouterLink from './RouterLinks';
 
@@ -17,8 +29,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MobileDrawer({ open, showDrawer }) {
   const styles = useStyles();
+  const router = useRouter();
+
+  function handleLinkClick(href) {
+    showDrawer(false);
+    router.push(href);
+  }
+
   return (
-    <Drawer
+    <SwipeableDrawer
       open={open}
       onClose={() => showDrawer(false)}
       anchor="left"
@@ -26,8 +45,31 @@ export default function MobileDrawer({ open, showDrawer }) {
         paper: styles.paper,
       }}
     >
-      <RouterLink
-        href="/about"
+      <List>
+        <ListItem onClick={() => handleLinkClick('/about')}>
+          <ListItemIcon>
+            <InsertEmoticonIcon />
+          </ListItemIcon>
+          <ListItemText>About Me</ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem onClick={() => handleLinkClick('/blog')}>
+          <ListItemIcon>
+            <DescriptionIcon />
+          </ListItemIcon>
+          <ListItemText>Blog</ListItemText>
+        </ListItem>
+        <Divider />
+        <ListItem onClick={() => handleLinkClick('/portfolio')}>
+          <ListItemIcon>
+            <BusinessCenterIcon />
+          </ListItemIcon>
+          <ListItemText>Portfolio</ListItemText>
+        </ListItem>
+        <Divider />
+      </List>
+      {/* <RouterLink
+        onClick={() => handleLinkClick('/about')}
         text="About Me"
         className={styles.link}
         variant="h5"
@@ -45,7 +87,7 @@ export default function MobileDrawer({ open, showDrawer }) {
         href="/portfolio"
         className={styles.link}
         variant="h5"
-      />
-    </Drawer>
+      /> */}
+    </SwipeableDrawer>
   );
 }
